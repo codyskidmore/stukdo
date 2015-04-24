33770fb7b0beb4bf88efca1b24e0d2f638fb4922
Cody Skidmore <cody@skidmore.us>
2015-04-24 08:39:49 -0400

    Added Task.title. Updated views to use task.title & added Edit/delete actions/w icons

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -79,6 +79,6 @@ class TasksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
-      params.require(:task).permit(:content, :state)
+      params.require(:task).permit(:content, :state, :title)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150422184102) do
+ActiveRecord::Schema.define(version: 20150424000608) do
 
   create_table "tasks", force: :cascade do |t|
     t.text     "content"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150422184102) do
     t.datetime "updated_at",                   null: false
     t.integer  "user_id"
     t.string   "state",      default: "to_do"
+    t.string   "title"
   end
 
   create_table "users", force: :cascade do |t|
